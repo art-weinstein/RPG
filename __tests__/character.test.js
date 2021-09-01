41a32a618fa9ac3bdab993896329b9cfb5fbbc91
Art Weinstein <artur.weinstein@gmail.com>
2021-09-01 15:16:10 -0700

    test function to assign character id to 1
    Co-authored-by: Kevin Funez <kevin.funez315@gmail.com>

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -1,18 +1,21 @@
 import { Character } from "../src/js/character";
 
-// Character
 describe('Character', () => {
   test('Should create character object' , () => {
-    let character = new Character();
+    let character = new Character(1, 0);
     expect(character.id).toEqual(1);
-    // expect(character.level).toEqual(1);
+    expect(character.experience).toEqual(0)
     expect(character.health).toEqual(100);
     expect(character.armor).toEqual(50);
     expect(character.strength).toEqual(1);
     expect(character.inventory).toEqual(0);
   });
   test ('Should set character level to 1', () =>{
-    let character = new Character();
+    let character = new Character(1, 0);
     expect(character.level()).toEqual(1);
-  })
+  });
+  test("should return id of 1", () => {
+    let character = new Character(1, 0);
+    expect(character.id).toEqual(1);
+  });
 });
--- src/js/character.js
+++ src/js/character.js
@@ -1,14 +1,23 @@
-export function Character (id, level, health, armor, strength, inventory) {
-  this.id = 1;
+export function Character (id, experience) {
+  this.id = id;
   // this.level = level;
   this.health = 100;
   this.armor = 50;
   this.strength = 1;
   this.inventory = 0;
+  this.experience = experience;
 };
 
+Character.prototype.assignId = function() {
+  let currentId = this.id;
+  currentId = 1
+};
 
 Character.prototype.level = function(){
-  this.level = 1;
-  return this.level;
+  let currentExperience = this.experience;
+  let currentLevel = 0;
+  if (currentExperience === 0){
+    currentLevel = 1;
+    return currentLevel;
+    };
 };
